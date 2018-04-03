db48c9a0440b60c2a77ea858553221fdb57032ac
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-04-03 16:18:30 +0200

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ClientsComponent } from './clients/clients.component';
+import { UxComponent } from './ux/ux.component';
+import { WebComponent } from './web/web.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'clients', component: ClientsComponent },
+  { path: 'ux', component: UxComponent },
+  { path: 'web', component: WebComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ClientsComponent } from './clients/clients.component';
+import { UxComponent } from './ux/ux.component';
+import { ContactComponent } from './contact/contact.component';
+import { WebComponent } from './web/web.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ClientsComponent,
+    UxComponent,
+    ContactComponent,
+    WebComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
