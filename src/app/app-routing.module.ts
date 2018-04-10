fbe207b45d0691a58748529ac05847c6e963a9d8
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-04-10 16:36:19 +0200

    add: todo6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,12 @@ import { WebComponent } from './web/web.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'clients', component: ClientsComponent },
   { path: 'ux', component: UxComponent },
   { path: 'web', component: WebComponent },
   { path: 'contact', component: ContactComponent },
-  { path: '', redirectTo: '', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
