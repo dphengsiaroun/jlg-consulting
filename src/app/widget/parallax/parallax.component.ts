fb3c549870a90ad833d8d83c45ecf39ecd1d57d0
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-05-03 12:15:17 +0200

    add: parallax

--- src/app/widget/parallax/parallax.component.ts
+++ src/app/widget/parallax/parallax.component.ts
@@ -3,8 +3,8 @@ import { LoadImageService } from '../load-image.service';
 
 @Component({
   selector: 'app-parallax',
-  templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss']
+  templateUrl: './parallax.component.html',
+  styleUrls: ['./parallax.component.scss']
 })
 export class ParallaxComponent implements OnInit, OnChanges {
 
@@ -22,8 +22,9 @@ export class ParallaxComponent implements OnInit, OnChanges {
         this.state = 0;
         await this.li.loadImage(this.smallImg);
         this.state = 1;
-        await this.li.loadImage(this.src);
-        this.state = 2;
+        console.log('smallImg', this.smallImg);
+        // await this.li.loadImage(this.src);
+        // this.state = 2;
       } catch (e) {
         console.error('Cannot load image', this.smallImg, this.src);
       }
